58572e7f335714da7c44c25dd7d0a226921f6f7f
Diego Pereira da Silva <diego.silva@it4solution.com.br>
2021-04-28 02:38:22 -0300

    ajustado js com api para consultar placa e mask

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -1,12 +1,13 @@
-$(function () {
+$(function() {
     $("#celular").mask("(00)00000-0000")
     $("#cep").mask("00000-000")
     $('#cpfUser').mask('000.000.000-00', { reverse: true });
     $('.cnpjEmitente').mask('00.000.000/0000-00', { reverse: true });
+    $("#placa").mask("AAA-9A99")
 });
 
 
-$(function () {
+$(function() {
     if ($('.cpfcnpjmine').val() != null) {
         if ($('.cpfcnpjmine').val() != "") {
             $(".cpfcnpjmine").prop('readonly', true);
@@ -21,17 +22,17 @@ $(function () {
 
 });
 
-$(function () {
-    var telefoneN = function (val) {
-        return val.replace(/\D/g, '').length > 10 ? '(00)00000-0000' : '(00)0000-00009';
-    },
+$(function() {
+    var telefoneN = function(val) {
+            return val.replace(/\D/g, '').length > 10 ? '(00)00000-0000' : '(00)0000-00009';
+        },
         telefoneOptions = {
-            onKeyPress: function (val, e, field, options) {
+            onKeyPress: function(val, e, field, options) {
                 field.mask(telefoneN.apply({}, arguments), options);
             },
         };
     $('#telefone').mask(telefoneN, telefoneOptions);
-    $('#telefone').on('paste', function (e) {
+    $('#telefone').on('paste', function(e) {
         e.preventDefault();
         var clipboardCurrentData = (e.originalEvent || e).clipboardData.getData('text/plain');
         $('#telefone').val(clipboardCurrentData);
@@ -39,18 +40,18 @@ $(function () {
 
 });
 
-$(function () {
+$(function() {
     // INICIO FUNÇÃO DE MASCARA CPF/CNPJ
-    var cpfMascara = function (val) {
-        return val.replace(/\D/g, '').length > 11 ? '00.000.000/0000-00' : '000.000.000-009';
-    },
+    var cpfMascara = function(val) {
+            return val.replace(/\D/g, '').length > 11 ? '00.000.000/0000-00' : '000.000.000-009';
+        },
         cpfOptions = {
-            onKeyPress: function (val, e, field, options) {
+            onKeyPress: function(val, e, field, options) {
                 field.mask(cpfMascara.apply({}, arguments), options);
             },
         };
     $('.cpfcnpj').mask(cpfMascara, cpfOptions);
-    $('.cpfcnpj').on('paste', function (e) {
+    $('.cpfcnpj').on('paste', function(e) {
         e.preventDefault();
         var clipboardCurrentData = (e.originalEvent || e).clipboardData.getData('text/plain');
         $('.cpfcnpj').val(clipboardCurrentData);
@@ -58,7 +59,7 @@ $(function () {
     // FIM FUNÇÃO DE MASCARA CPF/CNPJ
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
     if ($("[name='idClientes']").val()) {
         $("#nomeCliente").focus();
     } else {
@@ -146,7 +147,7 @@ $(document).ready(function () {
         return true;
     }
 
-    $('#buscar_info_cnpj').on('click', function () {
+    $('#buscar_info_cnpj').on('click', function() {
         //Nova variável "ndocumento" somente com dígitos.
         var ndocumento = $('#documento').val().replace(/\D/g, '');
 
@@ -168,7 +169,7 @@ $(document).ready(function () {
                 dataType: 'jsonp',
                 crossDomain: true,
                 contentType: "text/javascript",
-                success: function (dados) {
+                success: function(dados) {
                     if (dados.status == "OK") {
                         //Atualiza os campos com os valores da consulta.
                         if ($("#nomeCliente").val() != null) {
@@ -187,7 +188,7 @@ $(document).ready(function () {
                         $("#estado").val(dados.uf);
                         if (dados.complemento != "") {
                             $("#complemento").val(capital_letter(dados.complemento));
-                        } else{
+                        } else {
                             $("#complemento").val("");
                         }
 
@@ -221,7 +222,7 @@ $(document).ready(function () {
                         });
                     }
                 },
-                error: function () {
+                error: function() {
                     ///CEP pesquisado não foi encontrado.
                     if ($("#nomeCliente").val() != null) {
                         $("#nomeCliente").val("");
@@ -252,8 +253,209 @@ $(document).ready(function () {
         }
     });
 
+    // Busca Placa
+    function buscaPlaca2(placa) {
+        var ndocumento = placa.replace('-', '').toLocaleUpperCase();
+
+        if (validarPlaca(ndocumento)) {
+            //Preenche os campos com "..." enquanto consulta webservice.
+            $("#carro").val("...");
+            $("#montadora").val("...");
+            $("#anoFabricacao").val("...");
+
+            $("#anoModelo").val("...");
+            $("#cor").val("...");
+            $("#chassi").val("...");
+            $("#municipio").val("...");
+            $("#uf").val("...");
+            $("#status").val("...");
+
+            //Consulta o webservice receitaws.com.br/
+            $.ajax({
+                url: "https://apicarros.com/v2/consultas/" + ndocumento + "/f63e1e63dd231083d38ce929984aac7d",
+                //url: "https://apicarros.com/v1/consulta/" + ndocumento + "/json",
+                dataType: 'json',
+                crossDomain: true,
+                //contentType: "text/javascript",
+                success: function(dados) {
+                    if (dados.codigoRetorno == "0" || dados.codigoSituacao == "0") {
+                        //Atualiza os campos com os valores da consulta.
+                        carro = dados.modelo.substring(dados.modelo.indexOf("/") + 1);
+                        montadora = (dados.modelo.split("/")[0]);
+                        if (montadora == "" || montadora == null) {
+                            montadora = dados.marca;
+                        };
+
+                        $("#carro").val(capitalizeFirstLetter(carro));
+                        $("#montadora").val(capitalizeFirstLetter(montadora));
+                        $("#anoFabricacao").val(dados.ano);
+
+                        $("#anoModelo").val(dados.anoModelo);
+                        $("#cor").val(capitalizeFirstLetter(dados.cor));
+                        $("#chassi").val(dados.chassi);
+                        $("#municipio").val(capitalizeFirstLetter(dados.municipio));
+                        $("#uf").val(dados.uf);
+                        $("#status").val(capitalizeFirstLetter(dados.situacao));
+
+                        // Força uma atualizacao do endereco via cep
+                        //document.getElementById("cep").focus();
+                        //document.getElementById("nomeCliente").focus();
+                    } //end if.
+                    else {
+                        //Placa pesquisada não foi encontrada.
+                        $("#carro").val("");
+                        $("#montadora").val("");
+                        $("#anoFabricacao").val("");
+                        $("#anoModelo").val("");
+                        $("#cor").val("");
+                        $("#chassi").val("");
+                        $("#municipio").val("");
+                        $("#uf").val("");
+                        $("#status").val("");
+
+                        Swal.fire({
+                            type: "warning",
+                            title: "Atenção (" + ndocumento + ")",
+                            text: "Placa não encontrada."
+                        });
+                    }
+                },
+                error: function() {
+                    ///Placa pesquisada com erro.
+                    $("#carro").val("");
+                    $("#montadora").val("");
+                    $("#anoFabricacao").val("");
+                    $("#anoModelo").val("");
+                    $("#cor").val("");
+                    $("#chassi").val("");
+                    $("#municipio").val("");
+                    $("#uf").val("");
+                    $("#status").val("");
+
+                    Swal.fire({
+                        type: "warning",
+                        title: "Atenção (" + ndocumento + ")",
+                        text: "Erro: Placa não encontrada."
+                    });
+                },
+                timeout: 2000,
+            });
+        } else {
+            Swal.fire({
+                type: "warning",
+                title: "Atenção",
+                text: "Placa inválida!"
+            });
+        }
+    };
+    $('#buscar_info_placa').on('click', function() {
+        //Nova variável "ndocumento" somente com dígitos.
+        var ndocumento = $('#placa').val().replace('-', '').toLocaleUpperCase();
+        buscaPlaca2(ndocumento);
+        return;
+        if (validarPlaca(ndocumento)) {
+            //Preenche os campos com "..." enquanto consulta webservice.
+            $("#carro").val("...");
+            $("#montadora").val("...");
+            $("#anoFabricacao").val("...");
+
+            $("#anoModelo").val("...");
+            $("#cor").val("...");
+            $("#chassi").val("...");
+            $("#municipio").val("...");
+            $("#uf").val("...");
+            $("#status").val("...");
+
+            //Consulta o webservice receitaws.com.br/
+            $.ajax({
+                url: "https://apicarros.com/v1/consulta/" + ndocumento + "/json",
+                dataType: 'json',
+                crossDomain: true,
+                //contentType: "text/javascript",
+                success: function(dados) {
+                    if (dados.codigoRetorno == "0" || dados.codigoSituacao == "0") {
+                        //Atualiza os campos com os valores da consulta.
+                        carro = dados.modelo.substring(dados.modelo.indexOf("/") + 1);
+                        montadora = (dados.modelo.split("/")[0]);
+
+                        $("#carro").val(capitalizeFirstLetter(carro));
+                        $("#montadora").val(capitalizeFirstLetter(montadora));
+                        $("#anoFabricacao").val(dados.ano);
+                        $("#anoModelo").val(dados.anoModelo);
+                        $("#cor").val(dados.cor);
+                        $("#chassi").val(dados.chassi);
+                        $("#municipio").val(dados.municipio);
+                        $("#uf").val(dados.uf);
+                        $("#status").val(dados.situacao);
+
+                        // Força uma atualizacao do endereco via cep
+                        //document.getElementById("cep").focus();
+                        //document.getElementById("nomeCliente").focus();
+                    } //end if.
+                    else {
+                        //Placa pesquisada não foi encontrada.
+                        $("#carro").val("");
+                        $("#montadora").val("");
+                        $("#anoFabricacao").val("");
+                        $("#anoModelo").val("");
+                        $("#cor").val("");
+                        $("#chassi").val("");
+                        $("#municipio").val("");
+                        $("#uf").val("");
+                        $("#status").val("");
+
+                        Swal.fire({
+                            type: "warning",
+                            title: "Atenção (" + ndocumento + ")",
+                            text: "Placa não encontrada."
+                        });
+                    }
+                },
+                error: function() {
+                    ///Placa pesquisada com erro.
+                    $("#carro").val("");
+                    $("#montadora").val("");
+                    $("#anoFabricacao").val("");
+                    $("#anoModelo").val("");
+                    $("#cor").val("");
+                    $("#chassi").val("");
+                    $("#municipio").val("");
+                    $("#uf").val("");
+                    $("#status").val("");
+
+                    Swal.fire({
+                        type: "warning",
+                        title: "Atenção (" + ndocumento + ")",
+                        text: "Erro: Placa não encontrada."
+                    });
+                },
+                timeout: 2000,
+            });
+        } else {
+            Swal.fire({
+                type: "warning",
+                title: "Atenção",
+                text: "Placa inválida!"
+            });
+        }
+    });
+
+    function validarPlaca(placa) {
+        placa = placa.replace('-', '');
+
+        if (placa == '') return false;
+
+        if (placa.length != 7) return false;
+
+        // Elimina Placas invalidas conhecidas
+        if (placa == "AAA1234" ||
+            placa == "ABC1234")
+            return false;
+        return true;
+    };
+
     //Quando o campo cep perde o foco.
-    $("#cep").blur(function () {
+    $("#cep").blur(function() {
 
         //Nova variável "cep" somente com dígitos.
         var cep = $(this).val().replace(/\D/g, '');
@@ -276,7 +478,7 @@ $(document).ready(function () {
                 $("#estado").val("...");
 
                 //Consulta o webservice viacep.com.br/
-                $.getJSON("https://viacep.com.br/ws/" + cep.replace(/\./g, '') + "/json/?callback=?", function (dados) {
+                $.getJSON("https://viacep.com.br/ws/" + cep.replace(/\./g, '') + "/json/?callback=?", function(dados) {
 
                     if (!("erro" in dados)) {
                         //Atualiza os campos com os valores da consulta.
